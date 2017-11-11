9c37992edf13f5420ac46b429d561d16d87e8519
Kageetai <ma@kageetai.net>
2017-11-11 13:49:28 +0100

    recalculate sphere width on resize event

--- atmos.js
+++ atmos.js
@@ -1,6 +1,11 @@
 jQuery(function() {
     console.log('atmos-sphere');
+    atmosWidth();
+    jQuery(window).resize(atmosWidth);
+});
+
+function atmosWidth() {
     jQuery('.atmos-sphere').each(function(index, element) {
         jQuery(element).height(jQuery(element).width());
     });
-});
+}
